49d91e7eda9981f637d8c3ddf757676dfac30fc3
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2024-10-17 11:47:12 +0200

    aggiunto - a riga 4

--- EserciziAcademy/academy/esercizi/Esercizio_1_1.java
+++ EserciziAcademy/academy/esercizi/Esercizio_1_1.java
@@ -1,6 +1,6 @@
 package academy.esercizi;
 public class Esercizio_1_1 {
     public static void main(String[] args) {
-        System.out.println("BUON LAVORO!");
+        System.out.println("BUON LAVORO!-");
     }
 }
