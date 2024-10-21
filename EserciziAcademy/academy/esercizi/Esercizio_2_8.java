94de8dde116d3e104c2494320755e16cea42b90f
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2024-10-21 17:37:59 +0200

    Esercizio_2_8

---
+++ EserciziAcademy/academy/esercizi/Esercizio_2_8.java
@@ -0,0 +1,21 @@
+package academy.esercizi;
+
+import java.util.Random;
+
+public class Esercizio_2_8 {
+    public static void main(String[] args) {
+        int[] facceDado = new int[6];
+
+        Random random = new Random();
+
+        for (int i = 0; i < 1000; i++) {
+            int lancio = random.nextInt(6) + 1;
+            facceDado[lancio - 1]++;
+        }
+
+        System.out.println("Faccia - Frequenza");
+        for (int i = 0; i < facceDado.length; i++) {
+            System.out.println((i + 1) + "\t" + facceDado[i]);
+        }
+    }
+}
