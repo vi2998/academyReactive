981d6d22dad466232da5dfdcb55caa5e50284aa6
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2024-11-08 09:54:19 +0100

    Modifiche esercizio 30 e 31_2

--- EserciziAcademy/academy/esercizi/esercizio_30/Prova30.java
+++ EserciziAcademy/academy/esercizi/esercizio_30/Prova30.java
@@ -6,15 +6,18 @@ public class Prova30 {
     public static void main(String[] args) {
 
         // Collaudo FreeResponseQuestion
-    /*    Question q = new FreeResponseQuestion();
+        System.out.println("Collaudo FreeResponseQuestion");
+        Question q = new FreeResponseQuestion();
         q.setDomanda("Quale è la capitalia dell’Italia?");
         q.display();
         q.setRisposta("Roma");
         System.out.println(q.getRisposta());
         System.out.println(q.checkAnswer("Roma"));
-    */
+
+        System.out.println("---------------------------------------\n");
+
         // Collaudo FillInQuestion
-    /*
+        System.out.println("Collaudo FillInQuestion");
         Question fraseDaRiempire = new FillInQuestion();
         fraseDaRiempire.setDomanda("La capitale dell'Italia è ****, si trova nella regione **** ed ha **** abitanti");
         fraseDaRiempire.setDomanda("La capitale dell'Italia è ****, si trova nella regione **** ed ha **** abitanti");
@@ -24,20 +27,27 @@ public class Prova30 {
         String risposta = "Roma, Lazio, 2800000";
         boolean check = fraseDaRiempire.checkAnswer(risposta);
         String testoCompleto = fraseDaRiempire.getRisposta();
-
+        System.out.println(testoCompleto);
         System.out.println("check = " + check);
-    */
+
+        System.out.println("---------------------------------------\n");
+
         // Collaudo NumericQuestion
-    /*   Question numericQuestion = new NumericQuestion();
+        System.out.println("Collaudo NumericQuestion");
+
+        Question numericQuestion = new NumericQuestion();
         numericQuestion.setDomanda("Quanti anni hai?");
         numericQuestion.display();
         String eta = "5";
         numericQuestion.setRisposta(eta);
         System.out.println("numericQuestion.getRisposta() = " + numericQuestion.getRisposta());
         System.out.println("numericQuestion.checkAnswer() = " + numericQuestion.checkAnswer(eta));
-*/
+
+        System.out.println("---------------------------------------\n");
+
         // Collaudo ChoiceQuestion
-    /*    ChoiceQuestion rispostaAScelta = new ChoiceQuestion();
+        System.out.println("Collaudo ChoiceQuestion");
+        ChoiceQuestion rispostaAScelta = new ChoiceQuestion();
         rispostaAScelta.setDomanda("Chi ha ucciso l'uomo ragno?: \n");
         rispostaAScelta.setChoice("Daniele", 0, false);
         rispostaAScelta.setChoice("Mario", 1, false);
@@ -45,9 +55,11 @@ public class Prova30 {
         rispostaAScelta.display();
 
         System.out.println(rispostaAScelta.checkAnswer("Non si sa"));
-/*/
-        // Collaudo MultipleChoiceQuestion
 
+        System.out.println("---------------------------------------\n");
+
+        // Collaudo MultipleChoiceQuestion
+        System.out.println("Collaudo MultipleChoiceQuestion");
         ChoiceQuestion rispostaMultipla = new MultipleChoiceQuestion();
         rispostaMultipla.setDomanda("Quali sono città del Piemonte: \n");
         rispostaMultipla.setChoice("Asti", 0, true);
