3f5c604e229b32ac1536eaa83d1ba6fcda35dfd6
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2024-11-22 17:13:10 +0100

    inizio esercizio 37_7

---
+++ EserciziAcademy/academy/esercizi/esercizio_37/Esercizio_37_7.java
@@ -0,0 +1,36 @@
+package academy.esercizi.esercizio_37;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Esercizio_37_7 {
+    public static void main(String[] args) {
+        Esercizio_37_7 esercizio_37_7 = new Esercizio_37_7();
+        esercizio_37_7.test();
+
+        /* Scrivere un programma che legge un file di testo e visualizza un elenco contenente, in ordine alfabetico tutte le parole presenti nel file, seguite da un conteggio che indica il numero di ripetizioni
+        di ciascuna parola.
+
+        Scrivete un programma che legga un file di codice sorgente Java e generi un elenco di tutti gli identificatori presenti, visualizzando, accanto a ciascuno di essi, i numeri delle righe in cui compare.
+        Per semplicità considereremo che qualsiasi stringa costituita soltanto da lettere, cifre numeriche e caratteri di sottolineatura sia un identificatore.
+        Dichiarate la variabile Scanner in per leggere il file, ciclatela con nextLine e splittate ciascuna riga con il pattern "[^A-Za-z0-9_]+" in modo da ottenere un array di identificatori
+        ed il numero di righe contato in base alle volte in cui si richiama nextLine.
+         */
+    }
+
+    private void test() throws FileNotFoundException {
+        String filepath = "C:\\Users\\SVG.Pitrelli-cons\\Documents\\rootGit\\gitAcademy\\EserciziAcademy\\academy\\esercizi\\esercizio_1\\Esercizio_1_1.java";
+        File file = new File(filepath);
+        Scanner scanner = new Scanner(file);
+
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        while(scanner.hasNextLine()) {
+            int numeroDellaLinea = 0;
+            String linea = scanner.nextLine();
+            String[] paroleDellaLinea = linea.split("[^A-Za-z0-9_]]+");
+        }
+    }
+}
