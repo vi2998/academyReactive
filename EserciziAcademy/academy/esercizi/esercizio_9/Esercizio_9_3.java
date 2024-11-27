a1816c7c74590658d8aedec0f97bec4c2cac1ce9
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2024-11-27 10:58:08 +0100

    9_3 DA FIXARE

--- EserciziAcademy/academy/esercizi/esercizio_9/Esercizio_9_3.java
+++ EserciziAcademy/academy/esercizi/esercizio_9/Esercizio_9_3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Esercizio_9_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+// FIXARE SOTTO
         System.out.println("Inserisci coordinate punto A (x y): ");
         int[] verticeA = {scanner.nextInt(), scanner.nextInt()};
 
