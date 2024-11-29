e72d3c1efeb5a80b679666d448146c44a559bdd6
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2024-11-29 17:31:34 +0100

    Continuazione esercizio

--- esercizioTesting/src/test/java/it/reactive/esercizioTesting/businesslogic/ServiceAstaTest.java
+++ esercizioTesting/src/test/java/it/reactive/esercizioTesting/businesslogic/ServiceAstaTest.java
@@ -1,12 +1,40 @@
 package it.reactive.esercizioTesting.businesslogic;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 public class ServiceAstaTest {
 
+    ServiceAsta serviceAsta;
+
+
+    @Before
+    public void creaOggetto(){
+        serviceAsta = new ServiceAsta();
+    }
+
+    @Test
+    public void costruttore() {
+        serviceAsta = new ServiceAsta();
+        assertEquals("Partecipanti di default errati" ,
+                "Default", serviceAsta.getPartecipanti().get(0));
+    }
+
     @Test
     public void inizializza() {
+        List<String> partecipanti = new ArrayList<>();
+        partecipanti.add("Federico");
+        partecipanti.add("Gabriele");
+        partecipanti.add("Simona");
+
+        serviceAsta.inizializza(partecipanti);
+
+        assertEquals("Inizializzazione non corretta", partecipanti, serviceAsta.getPartecipanti());
+
     }
 
     @Test
