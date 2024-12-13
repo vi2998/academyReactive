9026b72c451ea5a0a487225c7ecafc336c413d21
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2024-12-13 18:42:21 +0100

    UPD

--- EserciziSQL/Primo esercizio word/libreria_DELETE.sql
+++ EserciziSQL/Primo esercizio word/libreria_DELETE.sql
@@ -1,8 +1,7 @@
 --delete from tabella e farlo per ogni tabella
+delete from Libri_Librerie;
+delete from Libri;
 delete from Autori;
 delete from Generi;
-delete from Libri_Librerie;
 delete from Librerie;
-delete from Libri;
 delete from CaseEditrici;
-
