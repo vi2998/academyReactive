d0ff58cc3487fa4406412c9d3af7a1600279b542
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2024-12-13 17:53:52 +0100

    upd

--- EserciziSQL/Primo esercizio word/libreria_SQL.sql
+++ EserciziSQL/Primo esercizio word/libreria_SQL.sql
@@ -10,7 +10,17 @@
 	and l.autore_cognome = a.cognome;
 
 --3.	Recuperare gli autori ed i libri riportando le colonne dei libri vuote se l’autore non ha scritto libri (con l’istruzione left outer join)
+	select a.nome, a.cognome, l.titolo from autori as a 
+	left join libri as l 
+	on l.autore_nome = a.nome 
+	and l.autore_cognome = a.cognome;
+
 --4.	Recuperare gli autori ed i libri riportando le colonne dei libri vuote se l’autore non ha scritto libri (con l’istruzione right outer join)
+	select a.nome, a.cognome, l.titolo from libri as l 
+	right join autori as a 
+	on l.autore_nome = a.nome 
+	and l.autore_cognome = a.cognome;
+
 --5.	Recuperare i libri e le case editrici visualizzando le colonne della casa editrice come null se non presente la relazione
 --6.	Recuperare i libri e le case editrici visualizzando le colonne dei libri come null se non presente la relazione
 --7.	Estrarre il conteggio dei libri per genere ed autore riportando le colonne genere, cognome e conteggio per tutti i libri scritti dopo il ’01-01-2021’
