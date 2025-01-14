2be7ca09dfa686fd0d6d1c4f952e3008edaff3c3
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2025-01-14 12:57:00 +0100

    upd

---
+++ torneoDemo/src/main/java/it/reactive/torneoDemo/repository/jdbcPreparedStatement/GiocatoreDaoImplJDBCPreparedStatement.java
@@ -0,0 +1,4 @@
+package it.reactive.torneoDemo.repository.jdbcPreparedStatement;
+
+public class GiocatoreDaoImplJDBCPreparedStatement {
+}
---
+++ torneoDemo/src/main/java/it/reactive/torneoDemo/repository/jdbcQueryPSC/GiocatoreDaoImplJDBCQueryPSC.java
@@ -0,0 +1,4 @@
+package it.reactive.torneoDemo.repository.jdbcQueryPSC;
+
+public class GiocatoreDaoImplJDBCQueryPSC {
+}
