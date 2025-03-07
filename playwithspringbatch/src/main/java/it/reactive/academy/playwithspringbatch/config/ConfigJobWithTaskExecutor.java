dda52809bc9299ef8b94507c193fb3780c02c94d
ITMASTER\SVG.Pitrelli-cons <SVG.Pitrelli-cons@reactive-almaviva.it>
2025-03-07 11:48:34 +0100

    pwsb: MultiThread con TaskExcecutor

--- playwithspringbatch/src/main/java/it/reactive/academy/playwithspringbatch/config/ConfigJobWithTaskExecutor.java
+++ playwithspringbatch/src/main/java/it/reactive/academy/playwithspringbatch/config/ConfigJobWithTaskExecutor.java
@@ -1,9 +1,9 @@
 package it.reactive.academy.playwithspringbatch.config;
 
 import it.reactive.academy.playwithspringbatch.EccezioneDimensioneErrata;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.SkipListener;
-import org.springframework.batch.core.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -16,80 +16,87 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 
-public class ConfigJobWithStepFaultTollerant {
+public class ConfigJobWithTaskExecutor {
 
-    public static final String PRIMOSTEP_CHUNK_TOLLERANT = "PRIMOSTEP_CHUNK_TOLLERANT";
-    public static final String PRIMOJOB_CHUNK_TOLLERANT = "PRIMOJOB_CHUNK_TOLLERANT";
+    public static final String PRIMOSTEP_CHUNK_TASKEXECUTOR = "PRIMOSTEP_CHUNK_TASKEXECUTOR";
+    public static final String PRIMOJOB_CHUNK_TASKEXECUTOR = "PRIMOJOB_CHUNK_TASKEXECUTOR";
     public static final int CHUNK_SIZE = 5;
-    public static final String SKIP_LISTENER = "SKIP_LISTENER";
+    public static final String READER_TASKEXECUTOR = "READER_TASKEXECUTOR";
+    public static final String THREAD_TASK_EXECUTOR = "THREAD_TASK_EXECUTOR" ;
+    public static final String LISTENER_JOB_TASKEXECUTOR = "LISTENER_JOB_TASKEXECUTOR";
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Bean(PRIMOJOB_CHUNK_TOLLERANT)
+    @Bean(PRIMOJOB_CHUNK_TASKEXECUTOR)
     public Job creaPrimoJobChunk(JobRepository jobRepository
-            , @Qualifier(PRIMOSTEP_CHUNK_TOLLERANT) Step step
+            , @Qualifier(PRIMOSTEP_CHUNK_TASKEXECUTOR) Step step
+            , @Qualifier(LISTENER_JOB_TASKEXECUTOR) JobExecutionListener listener
     ) {
-        return new JobBuilder(PRIMOJOB_CHUNK_TOLLERANT, jobRepository)
+        return new JobBuilder(PRIMOJOB_CHUNK_TASKEXECUTOR, jobRepository)
                 .start(step)
+                .listener(listener)
                 .build();
     }
 
-    // per salvare le informazioni dei record che sono andati in errore
-    @Bean(SKIP_LISTENER)
-    public SkipListener skipListener(){
-        return new SkipListener() {
+    @Bean(LISTENER_JOB_TASKEXECUTOR)
+    public JobExecutionListener listener(
+            @Qualifier(THREAD_TASK_EXECUTOR) ThreadPoolTaskExecutor taskExecutor
+    ){
+        return new JobExecutionListener() {
             @Override
-            public void onSkipInRead(Throwable t) {
-                SkipListener.super.onSkipInRead(t);
-            }
-
-            @Override
-            public void onSkipInWrite(Object item, Throwable t) {
-                SkipListener.super.onSkipInWrite(item, t);
-            }
-
-            @Override
-            public void onSkipInProcess(Object item, Throwable t) {
-                SkipListener.super.onSkipInProcess(item, t);
-                System.out.println("************************" + item);
+            public void afterJob(JobExecution jobExecution) {
+                JobExecutionListener.super.afterJob(jobExecution);
+                System.out.println("FINE!!!!!!!!!!");
+                taskExecutor.destroy();
             }
         };
     }
 
+    @Bean(THREAD_TASK_EXECUTOR)
+    public TaskExecutor taskExecutor(){
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(10); // numero di thread di base che vngono mantenuti nel pool anche se sono inattivi
+        taskExecutor.setMaxPoolSize(20); /* numero max di thread che possono essere creati nel pool.
+                                            Se il num. di thread è = a  maxPoolSize e ci sono più task da eseguire,
+                                            questi task verranno messi in coda fino a quando un thread non sarà disponibile */
+        return new ThreadPoolTaskExecutor();
+    }
 
-    @Bean(PRIMOSTEP_CHUNK_TOLLERANT)
+    @Bean(PRIMOSTEP_CHUNK_TASKEXECUTOR)
     public Step creaPrimoStepChunk(JobRepository jobRepository
             , PlatformTransactionManager transactionManager
-            , @Qualifier(SKIP_LISTENER) SkipListener skipListener
+            , @Qualifier(THREAD_TASK_EXECUTOR) TaskExecutor taskExecutor
     ) {
-        return new StepBuilder(PRIMOSTEP_CHUNK_TOLLERANT, jobRepository)
+        return new StepBuilder(PRIMOSTEP_CHUNK_TASKEXECUTOR, jobRepository)
                 .<String, String>chunk(CHUNK_SIZE, transactionManager)
                 .reader(new FlatFileItemReaderBuilder<String>()
-                        .name("READER_TOLLERANT")
-                        .resource(new FileSystemResource("file/nuovofile.txt"))
+                        .name(READER_TASKEXECUTOR)
+                        .resource(new FileSystemResource("file/file4taskexecutor.txt"))
                         .lineMapper(new PassThroughLineMapper())
                         .build()
                 )
                 .processor(new ItemProcessor<String, String>() {
                     @Override
                     public String process(String item) throws Exception {
-                        if (item.equals("aaaaaaaa")) throw new RuntimeException();
-                        if (item.length() == 5) throw new EccezioneDimensioneErrata();
                         return item;
                     }
                 })
                 .writer(new ItemWriter<String>() {
                     @Override
                     public void write(Chunk<? extends String> chunk) throws Exception {
-                        chunk.forEach(System.out::println);
+                        chunk.forEach(el -> logger.info("elemento: {}",el));
                     }
                 })
+                /*.taskExecutor(new SimpleAsyncTaskExecutor()) // venogno fatti in multithread: non vengono stampati i ordine
+                     sono stati creati 4 thread, 1 per ciascun chunk e le elaborazione sono state fatte parallelamente */
 
-                .faultTolerant().skipLimit(4) // al 4 errore fallisce
-                .skip(EccezioneDimensioneErrata.class)
-                .listener(skipListener)
+                //.taskExecutor(taskExecutor) non conviene usare quello nel contesto di spring. conviene usare il simple
                 .build();
     }
 }
